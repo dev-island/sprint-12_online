b4494a24fc1bf71a29845d15d3f835cd41858f8b
AJ Webb <webbushka@gmail.com>
2023-12-07 14:44:11 -1000

    Adds jsdoc and explanation comments

--- KnightsTravail/index.js
+++ KnightsTravail/index.js
@@ -1,6 +1,18 @@
+/**
+ * Constants defining the minimum and maximum boundaries of the chessboard.
+ */
 const min = 0;
 const max = 7;
+
+/**
+ * A map to keep track of visited squares.
+ */
 const visited = new Map();
+
+/**
+ * An array defining all possible moves for a knight in chess.
+ * Each sub-array represents the x and y offsets for a move.
+ */
 const possibleMoves = [
   [1, 2],
   [-1, 2],
@@ -12,31 +24,48 @@ const possibleMoves = [
   [-2, 1],
 ];
 
+/**
+ * Calculates all possible moves from a given position on the chessboard.
+ *
+ * @param {number[]} position - The current position [x, y] on the chessboard.
+ * @returns {number[][]} An array of possible new positions after the move.
+ */
 function getPossibleMoves([x, y]) {
   return possibleMoves.map(([xMove, yMove]) => {
     const newX = x + xMove;
     const newY = y + yMove;
 
+    // Check if the new position is within the bounds of the chessboard
     if (newX >= min && newX <= max && newY >= min && newY <= max) {
       return [newX, newY];
     }
   });
 }
 
+/**
+ * Creates and returns a square object with various properties and methods.
+ *
+ * @param {number[]} coords - The coordinates of the square [x, y].
+ * @param {any} parent - The parent square from which this square was reached.
+ * @returns {object} An object representing the square with its properties and methods.
+ */
 function square([sqX, sqY], sqParent) {
   const x = sqX;
   const y = sqY;
   let parent = sqParent;
   let moves = getPossibleMoves([x, y]).filter(Boolean);
 
+  // Define methods to get and set the parent square
   const getParent = () => parent;
   const setParent = (parentSq) => {
     parent ||= parentSq;
   };
 
+  // Define methods to get the square's name and available moves
   const getName = () => `${x}, ${y}`;
   const getMoves = () => moves;
 
+  // Store the square in the visited map if not already visited
   if (!visited.has(getName())) {
     const sq = { getParent, parent, getMoves, getName, setParent };
     visited.set(getName(), sq);
@@ -46,13 +75,15 @@ function square([sqX, sqY], sqParent) {
 
 /**
  * Calculates the shortest path for a knight on a chessboard from the start coordinates to the finish coordinates.
+ *
  * @param {number[]} startCoords - The coordinates of the starting position [x, y].
  * @param {number[]} finishCoords - The coordinates of the finishing position [x, y].
- * @returns {string[]} - The path of coordinates representing the shortest path from start to finish.
- * @throws {TypeError} - If startCoords or finishCoords are not arrays.
- * @throws {Error} - If the coordinates are out of bounds.
+ * @returns {string[]} The path of coordinates representing the shortest path.
+ * @throws {TypeError} If startCoords or finishCoords are not arrays.
+ * @throws {Error} If the coordinates are out of bounds.
  */
 export default function knightMoves(startCoords, finishCoords) {
+  // Validate input coordinates
   if (!Array.isArray(startCoords) || !Array.isArray(finishCoords)) {
     throw new TypeError('Start and Finish points must be an array');
   } else if (
@@ -62,24 +93,29 @@ export default function knightMoves(startCoords, finishCoords) {
       `The values may not be larger than ${max} or smaller than ${min}`
     );
   }
+
+  // Clear the visited map and initialize the finish square
   visited.clear();
   const finish = square(finishCoords, 'finish');
 
+  // Use a queue for breadth-first search
   let queue = [finish];
 
   let startName = startCoords.join(', ');
-  let revs = 0;
+
+  // Search for the start square using breadth-first search
   while (!queue.find((i) => i.getName() === startName)) {
-    revs++;
     const currSquare = queue.shift();
 
+    // Enqueue all possible moves from the current square
     const enqueueList = currSquare
       .getMoves()
       .map((mCoords) => square(mCoords, currSquare))
       .filter(Boolean);
     queue.push(...enqueueList);
   }
-  console.log({ revs });
+
+  // Retrace the path from start to finish
   const start = visited.get(startName);
 
   let path = [];
@@ -89,14 +125,13 @@ export default function knightMoves(startCoords, finishCoords) {
     currSq = currSq?.getParent?.();
   }
 
-  console.log(
-    `The shortest path was ${path.length - 1} move${
-      path.length - 1 > 1 ? 's' : ''
-    }!
+  // Log and return the shortest path
+  const message = `The shortest path was ${path.length - 1} move${
+    path.length - 1 > 1 ? 's' : ''
+  }!
   The moves were:
   ${path.join('\n')}
-    `
-  );
+    `;
 
-  return path;
+  return { path, message };
 }
--- KnightsTravail/index.test.js
+++ KnightsTravail/index.test.js
@@ -16,11 +16,32 @@ test('handles out of bounds range', (t) => {
 });
 
 test('test the path length', (t) => {
-  t.is(knightMoves([3, 3], [4, 3]).length - 1, 3);
-  t.is(knightMoves([3, 3], [7, 6]).length - 1, 3);
-  t.is(knightMoves([0, 0], [7, 7]).length - 1, 6);
-  t.is(knightMoves([7, 6], [0, 0]).length - 1, 5);
-  t.is(knightMoves([0, 1], [2, 2]).length - 1, 1);
-  t.is(knightMoves([3, 0], [3, 4]).length - 1, 2);
-  t.is(knightMoves([7, 1], [6, 1]).length - 1, 3);
+  t.is(knightMoves([3, 3], [4, 3]).path.length - 1, 3);
+  t.is(knightMoves([3, 3], [7, 6]).path.length - 1, 3);
+  t.is(knightMoves([0, 0], [7, 7]).path.length - 1, 6);
+  t.is(knightMoves([7, 6], [0, 0]).path.length - 1, 5);
+  t.is(knightMoves([0, 1], [2, 2]).path.length - 1, 1);
+  t.is(knightMoves([3, 0], [3, 4]).path.length - 1, 2);
+  t.is(knightMoves([7, 1], [6, 1]).path.length - 1, 3);
+});
+
+test('handles invalid input', (t) => {
+  t.throws(() => knightMoves('start', [4, 3]), { instanceOf: TypeError });
+  t.throws(() => knightMoves([1, 2], 'finish'), { instanceOf: TypeError });
+  t.throws(() => knightMoves(null, [4, 3]), { instanceOf: TypeError });
+  t.throws(() => knightMoves([1, 2], undefined), { instanceOf: TypeError });
+});
+
+test('handles non-integer input', (t) => {
+  t.throws(() => knightMoves([3.5, 3], [4, 3]), { instanceOf: TypeError });
+  t.throws(() => knightMoves([1, 2], [4, 3.5]), { instanceOf: TypeError });
+  t.throws(() => knightMoves([1, 2.5], [4, 3]), { instanceOf: TypeError });
+});
+
+test('handles non-array input', (t) => {
+  t.throws(() => knightMoves(3, [4, 3]), { instanceOf: TypeError });
+  t.throws(() => knightMoves([1, 2], { x: 4, y: 3 }), {
+    instanceOf: TypeError,
+  });
+  t.throws(() => knightMoves([1, 2], '4, 3'), { instanceOf: TypeError });
 });
